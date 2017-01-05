31445fa3ba4c1a1d50ae19b27ea0dfecc22c4b43
Jaemin Han <jaemin.han@nyu.edu>
2017-01-05 16:33:41 -0500

    prompt component update but why isn't this working

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,11 +1,19 @@
 var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
-var Home = React.createClass({
-  render: function () {
+function Home () {
     return (
-      <div> Hello from Home! </div>
+      <div className='jumbotron col-sm-12 text-center' style={transparentBg}>
+        <h1>Github Battlez</h1>
+        <p className='lead'>You fancy huh?</p>
+        <Link to='/playerOne'>
+          <button type='button' className='btn btn-lg btn-success'>Get Started</button>
+        </Link>
+
+      </div>
     )
   }
-});
 
 module.exports = Home;
--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,33 +1,59 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
+var Prompt = require('../components/Prompt');
 
 var PromptContainer = React.createClass ({
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
+  getInitialState: function () {
+    return {
+      username: ''
+    }
+  },
+
+  // this [function] is for users --- whatever they type in the input field
+  handleUpdateUser: function (e) {
+    this.setState({
+      username: e.target.value
+    });
+  },
+
+  handleSubmitUser: function (e) {
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState({
+      username: ''
+    });
+
+    //  if player one has a route param that is truthy that means we're on second route
+    // and we need to go to the battles page.
+    if (this.props.routeParams.playerOne) {
+      // console.log(this.context)
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username,
+        }
+      })
+    // if not, we're on first page (player one page) and still needs to go to player two page
+    } else {
+      // console.log(this.context)
+      this.context.router.push('/playerTwo/' + this.state.username)
+    }
+  },
+
   render: function () {
     // console.log(this)
     return (
-      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-        <h1>{this.props.route.header}</h1>
-        <div className="col-sm-12">
-          <form>
-            <div className="form-group">
-              <input
-                className="form-control"
-                placeholder="Github Username"
-                type="text" />
-            </div>
-
-            <div className="form-group col-sm-4 col-sm-offset-4">
-              <button
-                className="btn btn-block btn-success"
-                type="submit">
-                  Continue
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      <Prompt
+        onSubmitUser={this.handleSubmitUser}
+        onUpdateUser={this.handleUpdateUser}
+        header={this.props.route.header}
+        username={this.state.username} />
     )
   }
 });
 
-module.exports = PromptContainer
+module.exports = PromptContainer;
