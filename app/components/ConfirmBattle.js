87936ce58cdf6fdc57e254d39218f256d2b12086
Jaemin Han <jaemin.han@nyu.edu>
2017-01-06 17:48:25 -0500

    button link back and initial battle result page

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
+var Link = require('react-router').Link;
 
 function puke (object) {
   return <pre>{JSON.stringify(object, null, ' ')}</pre>
@@ -23,10 +24,14 @@ function ConfirmBattle (props) {
         </div>
         <div className='col-sm-8 col-sm-offset-2'>
           <div className='col-sm-12' style={styles.space}>
-          Initiate Battle Button
+            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>
+              Initiate Battle
+            </button>
           </div>
           <div className='col-sm-12' style={styles.space}>
-          Link to /playerone
+            <Link to='/playerOne'>
+              <button type='button' className='btn btn-lg btn-danger'>Reselect Player</button>
+            </Link>
           </div>
         </div>
       </div>
