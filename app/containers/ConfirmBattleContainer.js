2196a474125299ba0241960ba6efaed95453369e
Jaemin Han <jaemin.han@nyu.edu>
2017-01-06 16:28:38 -0500

    setup battle page and fetch github info

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -17,8 +18,16 @@ var ConfirmBattleContainer = React.createClass({
   // },
 
   componentDidMount: function () {
+    // console.log('componentDidMount')
     var query = this.props.location.query;
-    console.log('componentDidMount')
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function (players) {
+        // console.log('PLAYERS', players)
+        this.setState({
+          isLoading: false,
+          playerInfo: [players[0], players[1]]
+        })
+      }.bind(this))
     // fetch info from github and then update state
   },
 
