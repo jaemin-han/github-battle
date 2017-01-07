956410afdf03e93b88168a10be13bac607bdf07e
Jaemin Han <jaemin.han@nyu.edu>
2017-01-06 20:30:19 -0500

    render score on results page

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -8,6 +8,40 @@ function getUserInfo (username) {
   return axios.get('https://api.github.com/users/' + username + param);
 }
 
+function getRepos (username) {
+  //fetch usernames repos
+ return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+}
+
+function getTotalStars (repos) {
+  //calculate all the starts that the use has
+  return repos.data.reduce(function (prev, current) {
+    return prev + current.stargazers_count
+  }, 0)
+}
+
+function getPlayersData (player) {
+  //get repos
+  //get total stars
+  //return object with that data
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function (totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+function calculateScores (players) {
+  //return an array, after doing some fancy algorithms to determine a winner
+  return [
+    players[0].followers * 3 * players[0].totalStars,
+    players[1].followers * 3 * players[1].totalStars
+  ]
+}
+
 var helpers = {
   getPlayersInfo: function (players) {
     return axios.all(players.map(function (username) {
@@ -21,6 +55,15 @@ var helpers = {
       console.warn('Error in getPlayersInfo: ', err)
     })
     //fetch some data from Github
+  },
+
+  battle: function (players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function (err) {console.warn('Error in getPlayersInfo: ', err)})
   }
 };
 
